3277a61f2301bbac5e62cc4be22f62b3a2ad8318
g97iulio1609 <giulio97.leone@gmail.com>
2026-01-28 18:05:53 +0100

    chore: update build artifacts

--- dist/tools.d.ts
+++ dist/tools.d.ts
@@ -1,2 +1,2 @@
 import 'zod';
-export { aQ as DeepResearchInput, aP as DeepResearchInputSchema, aR as DeepResearchTool, J as createDeepResearchTools } from './tools-DnTcE91c.js';
+export { aQ as DeepResearchInput, aP as DeepResearchInputSchema, aR as DeepResearchTool, J as createDeepResearchTools } from './tools-BI0yyGco.js';
